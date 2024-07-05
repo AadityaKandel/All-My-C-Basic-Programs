d145a070afa3b705f58f6af1a9b8c9c9526ef15b
The_Name_Is_AK <71229713+AadityaKandel@users.noreply.github.com>
2024-07-05 17:58:43 +0545

    Update find_primeNumber.c

--- find_primeNumber.c
+++ find_primeNumber.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
+/** 
+Turns out the first program I made, had a flaw where it was only able to calculate prime numbers
+up to a certain range. I simply mean that the logic produced wrong output if I tried to find 
+large prime numbers. This is the updated and corrected version. I've tested it and it produces no flaw.
+But still, this is simply the basic program and with programming, you can achieve even more optimized
+version of this program. Thank You
+**/
+
 int main(){
-    // Program to find prime number from 1 to 100
-    int i,n,count;
-    for(i=2;i<=100;i++){
+    int range, count;
+    printf("Enter the range of prime number below\n");
+    scanf("%d",&range);
+
+    for(int i=2; i<=range; i++){
         count=0;
-        for(n=1;n<=9;n++){
-            if(i%n==0 && i>n){
+        for(int z=1; z<i; z++){
+            if(i%z==0){
                 count++;
             }
         }
