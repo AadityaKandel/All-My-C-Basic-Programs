964f1a399da82be95551abe8906602e1cffd9616
The_Name_Is_AK <71229713+AadityaKandel@users.noreply.github.com>
2024-07-06 15:26:16 +0545

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,49 @@
+// Array Style
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    int series[n];
+    series[0] = 0;
+    series[1] = 1;
+
+    /*
+    The Fibonacci sequence is a sequence where the next term is the sum of the previous
+    two terms. The first two terms of the Fibonacci sequence are 0 followed by 1.
+    The Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21
+    */
+    printf("%d\t%d\t", series[0], series[1]);
+
+    for(int i = 2; i < n-1; i++){
+        series[i] = series[i-1] + series[i-2];
+        printf("%d\t", series[i]);
+    }
+
+    return 0;
+}
+
+// Non-Array Style
+#include <stdio.h>
+int main(){
+    int n, n1, n2,n3;
+    printf("Enter the number of terms\n");
+    scanf("%d",&n);
+
+    n1=0;
+    n2=1;
+
+
+    if (n>=1){printf("%d\t",n1);}
+    if (n>=2){printf("%d\t",n2);}
+
+    for(int i=2;i<n;i++){
+        n3=n2;
+        n2=n1+n2;
+        n1=n3;
+        printf("%d\t",n2);
+    }
+    return 0;
+}
