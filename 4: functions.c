5cacada25ae6c6799dca37b747b47d95555470c5
The_Name_Is_AK <71229713+AadityaKandel@users.noreply.github.com>
2024-07-10 20:16:48 +0545

    Update 4: functions.c

--- 4: functions.c
+++ 4: functions.c
@@ -46,6 +46,70 @@ Points to remember
   the only function that runs at first no matter how many functions you put above it. All the other functions must be called for them to work.
 */
 
+/*
+Lets talk about types of functions so we can understand this topic more clearly.
+
+Note: Before understanding types of functions, we first need to understand what's a calling function!?
+To understand, we need to know that a calling function is a function that calls another function.
+As an example:
+#include <stdio.h>
+
+int sum(int x, int y){
+  printf("%d",x+y);
+}
+
+int main(){
+  sum(3,4);
+  return 0;
+}
+
+In the above example, the calling function is "main()" function since "main()" is calling "sum()" function to do the addition of 3 & 4 numbers.
+You can simply translate the code in English to understand it further. Whenever you call your friend, you become the caller and your friend becomes the reciever.
+Similarly, in functions, the calling function is "main()" because we are calling "sum()" function from the "main()" function.
+The function from which you call another function is the calling function. Lets move to "Types of Functions"
+
+There are 4 types of functions and they are:
+1: Return type function with no arguments
+2: Return type function with arguments
+3: No Return type function with arguments
+4: No Return type function with no arguments
+
+What exactly are these? To understand, lets define each with syntax;
+
+1: Return type function with no arguments
+The return type function with no argument is such a function that takes no argument but still returns a value or variable or calculation to the calling function.
+2: Return type function with arguments
+The return type function with arguments is such a function that takes one or more arguments and returns a value or variable or calculation to the calling function.
+Note: Both of the above mentioned function can be declared with data type of "int" or "float" or "char" or "long" or more. But they cannot be defined with "void"
+
+3: No Return type function with no arguments
+The no return type function with no arguments is a function that neither take any arguments nor returns any value or variable or calculation to the calling function.
+4: No Return type function with arguments
+The no return type function with arguments is a function that takes arguments but doesn't return any value or variable or calculation to the calling function.
+Note: Both of the above mentioned function can be declared only with "void". It cannot be a "int" or "char" or "long" function since it is a no return type.
+
+Lets clarify with example:
+// Return type function with argument
+#include <stdio.h>
+
+int sum(int x, int y){
+  return x+y;
+}
+
+int main(){
+  printf("Sum of 4 and 5 is %d", sum(5,4));
+  return 0;
+}
+
+In the above example, the main() is calling sum() and providing two numbers i.e 5 & 4 which is being used by sum() function as x and y arguments.
+After that, the sum() function is returning x+y (i.e 5+4) to the main() function.
+To understand it even further, lets consider one example:
+Suppose the main function is you and the sum function is your friend. If you (main function) ask your friend (sum function) to find the sum of 5 & 4, s/he will
+say that the answer is 9. Hence, we can say that you called your friend and asked him the sum of 5 & 4 and he returned 9 as an answer. Same goes for functions.
+
+[Further example will be explained later...]
+*/
+
 #include <stdio.h>
 
 int sum(int x, int y){
