1c5ecf9bb21c3ec210d37a57406eba93b0a8cc90
The_Name_Is_AK <71229713+AadityaKandel@users.noreply.github.com>
2024-08-03 09:39:06 +0545

    Update 6: structure.c

--- 6: structure.c
+++ 6: structure.c
@@ -12,6 +12,95 @@ struct structure_name{
 struct structure_name var1, var2, var3, var_n; 
 // This can be either declared within the function or outside the function
 // Declaring outside the function makes it globally accessible & within makes the opposite
+
+It can also be defined in this way:
+    Syntax:
+struct structure_name{
+        data_type var1
+        data_type var2
+        data_type var3
+        data_type var-n
+}var1, var2, var3, var_n;
+
+Both of these ways depends upon the user and their preference.
+
+I'll directly move towards the most asked question of the difference between structure & arrays because most student
+ask why to use structure if there are arrays.
+
+First off, What is an array?
+An array is a collection of different variables of the same data type. Each array variable can contain different or same
+values depending upon the input but each values will be stored in a different array variable. This makes it easy to store
+large number of variables without having to declare them separately.
+
+Secondly, What is a structure?
+A structure is a collection of different variables of the different data type. This makes it even more efficient than arrays
+since arrays can only store one data type while structure can store as many data type as possible on a single go.
+
+Why structure over array?
+Arrays can be used on cases where muliple variables of the same data type are required.
+
+// If a question asks you to store salary of 40 employees only then you'd use array in this scenario.
+Eg:
+int salary[40];
+
+While structure can be used on cases where multiple information is given to be stored where each information is different
+than other.
+
+// If a question asks you to store name, address, dob, id, salary of 40 employee then you'd use structure.
+Eg:
+struct employee{
+    char name[50];
+    char address[100];
+    int dob,id,salary;
+}e[40];
+
+
+Third is the syntax. Syntax for an array is
+data_type variable_name[array_length];
+
+Syntax for a structure is
+struct structure_name{
+    data_type var1;
+    data_type var2;
+    data_type varn;
+}struct_var1, var2, varn;
+
+Fourth is, structure can have multiple syntax for multiple scenarios where,
+Syntax 1:
+struct structure_name{
+    data_type var1;
+    data_type var2;
+    data_type varn;
+}struct_var1, var2, varn;
+
+Syntax 2:
+struct structure_name{
+    data_type var1;
+    data_type var2;
+    data_type varn;
+}struct_var[structure_length];
+
+Syntax 3:
+struct structure_name{
+    data_type var1;
+    data_type var2;
+    data_type varn;
+};
+
+struct structure_name var1, var2, varn;
+
+where each syntax has different way of using it. While they have the same output, they can have multiple use cases.
+
+But, array has single syntax where "data_type variable_name[array_length" is the only one.
+And it also has a single use case...
+
+Fifth is the keywords where,
+structure has keywords called "struct"
+but array doesn't have a keyword.
+
+Sixth is the example of a simple program which you can give using structure & arrays.
+
+
 */
 
 struct Employee{
