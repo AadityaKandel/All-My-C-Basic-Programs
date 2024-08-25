f4bdd5d2f85079d70373e2b70ee281802e8a5d44
The_Name_Is_AK <71229713+AadityaKandel@users.noreply.github.com>
2024-08-25 14:30:35 +0545

    Create C_questions.c

---
+++ C_questions.c
@@ -0,0 +1,60 @@
+/*
+
+These are the questions from each Chapter that I've covered.
+
+A: nested if
+Write a C program to input two numbers, ask the user to select two modes: R & S. (R means Rectangle & S means Square)
+After either of the mode is selected, ask the user to select another two modes: A & P. (A means Area & P means Perimeter)
+Finally, display the respective information based on the modes selected.
+
+B: switch (NO LOOPS ALLOWED)
+Implement a switch program that lets the user select on the following modes and displays the respective logic
+and exits after each prompt
+1: Profit & Loss Finder
+2: Area of Rectangle Finder
+3: Area of Square Finder
+
+C: do-while loop
+Write a C program by combining switch and do while loop where it displays the following options, performs the respective
+logic and exits only when the respective option is selected
+1: Profit & Loss Finder
+2: Area of Rectangle Finder
+3: Area of Square Finder
+4: Exit
+
+D: while loop
+WAP to display the maximum value of an integer variable using just one variable and a while loop. 
+Answer must be: 2147483647
+
+E: for loop
+WAP to input a value, multiply it by itself until it reaches less than 0 and display the maximum limit of that value.
+You can use three variables. Hint: Temporay Storage!
+
+F: Functions
+WAP to use recursive function to print a fibonacci series up to the 15th term without using a loop.
+
+G: Array
+WAP to use a single array to store employee count, id, salary and phone number of 10 employees and display them in a tabular form.
+
+H: Structure
+WAP to store name, address, contact, id and salary of 10 employees and display their details by sorting their salary in ascending order.
+
+I: Pointer
+WAP to print the multiplication table of a given number using pointer and a void function.
+Rule: Only 1 variable for asking input can be used; another variable can either be a variable for a loop or a pointer or both.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
